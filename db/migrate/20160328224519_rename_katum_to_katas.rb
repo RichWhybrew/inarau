fd308d6dcd71e02b20de89598120097e7b1497cb
Rich Whybrew <richwhybrew@gmail.com>
2016-05-02 15:59:35 -0700

    removed bad migration files

--- db/migrate/20160328224519_rename_katum_to_katas.rb
+++
@@ -1,9 +0,0 @@
-class RenameKatumToKatas < ActiveRecord::Migration
-	def self.up
-  		rename_table :katum, :katas
-	end
-
-	def self.down
-		rename_table :katas, :katum
-	end
-end
--- db/migrate/20160329232853_remove_katum_id_from_vocabs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveKatumIdFromVocabs < ActiveRecord::Migration
-  def change
-    remove_column :vocabs, :katum_id, :integer
-  end
-end
